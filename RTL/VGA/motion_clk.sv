d3bd0274edb04cb02491d31b2d443ed2b43e4b98
razcarmeli <razcarmeli@gmail.com>
2026-06-19 21:51:33 +0300

    LFSR working

---
+++ RTL/VGA/motion_clk.sv
@@ -0,0 +1,41 @@
+
+module	motion_clk	(	
+					input		logic	clk,
+					input		logic startOfFrame,
+					input		logic	resetN,
+		 
+
+			  
+				   output	logic	motion_pulse
+
+);
+
+logic [4:0] counter;
+parameter int frames_per_motion = 10;
+
+always_ff@(posedge clk or negedge resetN)
+begin
+	if(!resetN) begin
+		counter <= 5'h0;
+		motion_pulse <= 1'b0;
+
+	end
+	
+	else begin
+		motion_pulse <= 1'b0;
+		
+		if (startOfFrame) begin
+			counter <= counter + 1'b1;
+			
+			if (counter == frames_per_motion - 1) begin
+			
+				counter <= 5'h0;
+				motion_pulse <= 1'b1;
+			end
+		end
+			
+	end
+end	
+endmodule
+
+
